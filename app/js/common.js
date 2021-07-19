564dce49bb012da486056e812ffd467825eb6101
Merak <mesheryakov97@gmail.com>
2021-07-19 21:28:27 +0500

    $fix: slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   //input mask phone
   $("input[name*='phone']").inputmask("+7 (999) 999-99-99");
 
@@ -15,18 +15,18 @@ $(function() {
     midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
   });
 
-  $(".open-service").click(function (){
-    let serviceName =  $(this).parent().children('.service__name').text();
+  $(".open-service").click(function () {
+    let serviceName = $(this).parent().children('.service__name').text();
     $('#call-service .popup-call__title span').text(serviceName);
   });
 
-  $(".open-single-service").click(function (){
-    let serviceName =  $(this).parent().parent().children('.banner__title').text();
+  $(".open-single-service").click(function () {
+    let serviceName = $(this).parent().parent().children('.banner__title').text();
     $('#call-service .popup-call__title span').text(serviceName);
   });
-   
 
-  
+
+
 
   //burger
   $(".burger").click(function () {
@@ -35,75 +35,75 @@ $(function() {
     $(".mobile-menu").toggleClass('active');
     $('body').toggleClass('fixed');
   });
-  
+
   $(".mobile-menu__url").click(function () {
     $(".mobile-menu").removeClass('active');
     $(".mobile-menu").fadeToggle();
     $('body').removeClass('fixed');
     $(".burger").removeClass("active");
   });
- 
+
 
   //sliders
 
   $('.works-slider').owlCarousel({
-    dots: false, 
+    dots: false,
     responsive: {
-        0:{
-            items:1, 
-            nav: false,
-            stagePadding: 40,
-            margin: 20, 
-        }, 
-        1200:{
-            nav: true,
-            items:3,
-            margin:40,
-            loop: true,
-        }
+      0: {
+        items: 1,
+        nav: false,
+        stagePadding: 40,
+        margin: 20,
+      },
+      1200: {
+        nav: true,
+        items: 3,
+        margin: 40,
+        loop: true,
+      }
     }
   });
 
   $('.specialists-slider').owlCarousel({
-    dots: false, 
+    dots: false,
     responsive: {
-        0:{
-            items:1,
-            nav: false,
-            stagePadding: 65,
-            margin: 20, 
-        }, 
-        1200:{
-            nav: true,
-            items:4,
-            margin:40,
-            loop: true,
-        }
+      0: {
+        items: 1,
+        nav: false,
+        stagePadding: 65,
+        margin: 20,
+      },
+      1000: {
+        nav: true,
+        items: 4,
+        margin: 40,
+        loop: true,
+      }
     }
   });
 
   $('.reviews-slider').owlCarousel({
-    dots: false, 
+    dots: false,
     responsive: {
-        0:{
-            items:1,
-            nav: false,
-            stagePadding: 40,
-            margin: 20, 
-        }, 
-        1200:{
-            nav: true,
-            items:2,
-            margin:40,
-            loop: true,
-        }
+      0: {
+        items: 1,
+        nav: false,
+        stagePadding: 40,
+        margin: 20,
+      },
+      1200: {
+        nav: true,
+        items: 2,
+        margin: 40,
+        loop: true,
+      }
     }
   })
 
   $(".fancybox").fancybox();
 
 
-  $('.services__more-btn').click(function(e){
+  $('.services__more-btn').click(function (e) {
     e.preventDefault();
     $(".services .service:nth-child(n+9)").addClass('active');
     $(".services__more-btn").hide();
